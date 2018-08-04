4d0cf66ab666e2e798b895bcfd26a8f037f4468b
renguangli <211887977@qq.com>
2018-08-05 01:10:59 +0800

    解决war包不能运行的问题

--- src/main/java/com/renguangli/rportal/RportalApplication.java
+++ src/main/java/com/renguangli/rportal/RportalApplication.java
@@ -3,15 +3,22 @@ package com.renguangli.rportal;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.renguangli.rportal.mapper"})
-public class RportalApplication {
+public class RportalApplication extends SpringBootServletInitializer {
 
     public static ApplicationContext ctx;
 
 	public static void main(String[] args) {
         ctx = SpringApplication.run(RportalApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(RportalApplication.class);
     }
 }
